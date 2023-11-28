837a3e3f623067e0f8c644dd2a18d1f1503a57bd
Samia8Asim <samia2003.8@gmail.com>
2023-11-28 12:58:45 +0200

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    out_str = ""
+    for i in range(len(str)):
+        if 97 <= ord(str[i]) <= 122:
+            out_str += "{}".format(chr(ord(str[i]) - ord('a') + ord('A')))
+        else:
+            out_str += "{}".format(str[i])
+    print("{}".format(out_str), end="\n")
