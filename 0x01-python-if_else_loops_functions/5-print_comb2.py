7eaa52bcff04a30373ddcda714ab2258628a2713
Samia8Asim <samia2003.8@gmail.com>
2023-11-28 12:19:50 +0200

    fiz

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if 0 <= i <= 98:
-        print("{:02}, ".foemat(i), end="")
+        print("{:02}, ".format(i), end="")
     elif i == 99:
         print("{:02}".format(i))
