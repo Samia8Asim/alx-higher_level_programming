bb175a9949e8389ca19c04c8916c35fa04ab2f16
Samia8Asim <samia2003.8@gmail.com>
2023-11-29 09:57:41 +0200

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 for code in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(code)) if (code % 2) == 0 else "{}".format(chr(code - 32)), end="")
+    print(
+        "{}".format(chr(code)) if (code % 2) == 0
+        else "{}".format(chr(code - 32)),
+        end=""
+    )
