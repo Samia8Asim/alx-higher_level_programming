0d982ef23e1110eabe88a6a3d26d6b3232539740
Samia8Asim <samia2003.8@gmail.com>
2023-11-30 14:44:04 +0200

    1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,12 @@ arg_len = len(arg_list)
 
 if arg_len == 0:
     print("{} arguments.".format(arg_len))
-elif arg_len >= 1:
+
+elif arg_len == 1:
+    print("{} argument:".format(arg_len))
+    print("1: {}".format(arg_list[0]))
+
+elif arg_len > 1:
     print("{} arguments:".format(arg_len))
     for i in range(arg_len):
         print("{}: {}".format(i + 1, arg_list[i]))
