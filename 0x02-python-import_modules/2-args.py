d40f3090d7d0d5ef3f07a49c6ceab01b92ccf8c9
Samia8Asim <samia2003.8@gmail.com>
2023-11-30 14:47:05 +0200

    b

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-arg_list = sys.argv[1:]
-arg_len = len(arg_list)
+if __name__ == "__main__":
+    arg_list = sys.argv[1:]
+    arg_len = len(arg_list)
 
-if arg_len == 0:
-    print("{} arguments.".format(arg_len))
+    if arg_len == 0:
+        print("{} arguments.".format(arg_len))
 
-elif arg_len == 1:
-    print("{} argument:".format(arg_len))
-    print("1: {}".format(arg_list[0]))
+    elif arg_len == 1:
+        print("{} argument:".format(arg_len))
+        print("1: {}".format(arg_list[0]))
 
-elif arg_len > 1:
-    print("{} arguments:".format(arg_len))
-    for i in range(arg_len):
-        print("{}: {}".format(i + 1, arg_list[i]))
+    elif arg_len > 1:
+        print("{} arguments:".format(arg_len))
+        for i in range(arg_len):
+            print("{}: {}".format(i + 1, arg_list[i]))
