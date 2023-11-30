820e05d7c156a7b322d4d0a48c5d2cac8865478f
Samia8Asim <samia2003.8@gmail.com>
2023-11-30 15:51:38 +0200

    m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,6 @@ import hidden_4
 if __name__ == "__main__":
     hidden = dir(math)
     names = [name for name in hidden if not name.startswith('__')]
-    print(name)
+    sorted_names = sorted(names)
+    for i in sorted_names:
+        print("{}".format(sorted_names[i]))
