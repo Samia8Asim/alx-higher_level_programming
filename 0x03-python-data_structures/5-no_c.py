4ff952d41405a79d12fb9a650ab0347a515f849c
Samia8Asim <samia2003.8@gmail.com>
2023-12-03 09:50:16 +0200

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     copy = ""
 
-    for ch in my_string:
-        if ch != "c" and ch != "C":
-            copy += ch
+    for ch in range(len(my_string)):
+        if (my_string[ch] != "c" and my_string[ch] != "C"):
+            copy += my_string[ch]
     return copy
