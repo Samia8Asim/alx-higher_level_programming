2155997af6b89721518d59030277a9e16e461a22
Samia8Asim <samia2003.8@gmail.com>
2023-12-04 09:31:26 +0200

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    new = []
+    for i in range(len(my_list)):
+        if (my_list[i] % 2) == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return new
