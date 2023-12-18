92d54e4d3b3028af8c3ef005deeba0893f792d40
Samia8Asim <samia2003.8@gmail.com>
2023-12-18 11:29:21 +0200

    1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        int_val = int(value)
+        print("{:d}".format(int_val))
+        return True
+    except ValueError:
+        return False
