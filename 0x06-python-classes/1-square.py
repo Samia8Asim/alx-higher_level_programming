787a10282474b9c72d4e7cf706bd013948a420c7
Samia8Asim <samia2003.8@gmail.com>
2023-12-19 12:10:14 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square:
+    """Square define"""
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: Square length
+        """
+        self.__size = size
