351c5a24d4f3d97ce9bba4193e08903bdfd9965a
Samia8Asim <samia2003.8@gmail.com>
2024-01-08 11:55:08 +0200

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""lookup module"""
+
 
 def lookup(obj):
+    """lookup func
+    Args:
+        obj: input object
+    Return:
+        a list object
+    """
     return dir(obj)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Rectangle module"""
+
 
 BaseGeometry = __import__(f'7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """Rectangle class"""
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
