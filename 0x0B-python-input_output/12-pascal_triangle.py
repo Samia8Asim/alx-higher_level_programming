7176cfb127bed5303a7eb90c531ba26329f532f4
Samia8Asim <samia2003.8@gmail.com>
2024-01-09 11:42:23 +0200

    12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal’s triangle"""
+
+
+def pascal_triangle(n):
+    from math import factorial as fac
+    pas_list=[]
+    if n <= 0:
+        return pas_list
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = pas_list[i-1][j-1] + pas_list[i-1][j]
+        pas_list.append(row)
+    return pas_list
