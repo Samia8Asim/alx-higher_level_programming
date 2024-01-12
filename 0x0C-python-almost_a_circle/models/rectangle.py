5f5aa81808a7a3eafb8f3fdae7cc65579bcecbcb
Samia8Asim <samia2003.8@gmail.com>
2024-01-12 15:45:28 +0200

    1-4

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.val_pos_int("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.val_pos_int("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.val_non_n_int("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.val_non_n_int("y", value)
+        self.__y = value
+
+    def val_pos_int(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer.".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0.".format(name))
+
+    def val_non_n_int(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer.".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0.".format(name))
+
+    def area(self):
+        return self.width * self.hight
