64c5f89b7699fefc7a58bdfc6aeb3cc24a9e2cb5
Samia8Asim <samia2003.8@gmail.com>
2024-01-15 16:05:10 +0200

    19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """base class module"""
 import json
+import os
 
 
 class Base:
@@ -14,8 +15,49 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        fname = cls.__name__ + ".json"
+        jstr = cls.to_json_string(
+                [obj.to_dictionary() for obj in list_objs])
+        with open(fname, 'w') as fh:
+            fh.write(jstr)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            instance = cls(1)
+        else:
+            raise NotImplementedError(
+                    f"Class {cls.__name__} not supported in "
+                    "create method")
+        instance.update(**dictionary)
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        fname = cls.__name__ + ".json"
+        if not os.path.exists(fname):
+            return []
+        with open(fname, 'r') as fh:
+            jdata = fh.read()
+        dict_list = cls.from_json_string(jdata)
+        instances = [cls.create(**d) for d in dict_list]
+        return instances
