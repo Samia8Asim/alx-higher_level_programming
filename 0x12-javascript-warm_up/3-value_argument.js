8ef47bb510997ed4d7e24ffeb0c0dc453ae84fc0
Samia8Asim <samia2003.8@gmail.com>
2024-03-11 16:51:04 +0200

    0

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // prints the first argument passed to the script
 
-const numArg = process.argv.length - 2;
-if (numArg === 0) {
+if (!(process.argv[2])) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
