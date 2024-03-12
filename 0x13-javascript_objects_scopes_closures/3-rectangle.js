08648c2ad1bafdc1b43e1961dfd8fde8b8cabb08
Samia8Asim <samia2003.8@gmail.com>
2024-03-12 13:42:54 +0200

    0

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (h <= 0 || w <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Rectangle;
