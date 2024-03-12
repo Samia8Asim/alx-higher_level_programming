3fb943e8e2fbe38b7dafb48de9b1a737ee0cdb85
Samia8Asim <samia2003.8@gmail.com>
2024-03-12 04:02:58 +0200

    0

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  prints My number: <first argument converted in integer>
+
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+	console.log(words[i]);
+}
