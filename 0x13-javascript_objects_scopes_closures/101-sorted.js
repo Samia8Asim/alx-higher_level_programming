b96170ae2acc7baca7c202a73b9644a1e572c4f7
Samia8Asim <samia2003.8@gmail.com>
2024-03-13 16:41:06 +0200

    0

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-// script that imports a dictionary of occurrences by user id 
+// script that imports a dictionary of occurrences by user id
 // and computes a dictionary of user ids by occurrence.
 
 const data = require('./101-data');
-newDict = {};
+const newDict = {};
 
 for (const id in data.dict) {
-	const count = data.dict[id];
+  const count = data.dict[id];
 
-	if (count in newDict) {
-		newDict[count].push(id);
-	} else {
-		newDict[count] = [id];
-	}
+  if (count in newDict) {
+    newDict[count].push(id);
+  } else {
+    newDict[count] = [id];
+  }
 }
 
 console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,19 +6,19 @@ const f2Path = process.argv[3];
 const destPath = process.argv[4];
 
 fs.readFile(f1Path, 'utf-8', (err, data1) => {
-	if (err) {
-		process.exit(1);
-	}
-	fs.readFile(f2Path, 'utf-8', (err, data2) => {
-		if (err) {
-			process.exit(1);
-		}
+  if (err) {
+    process.exit(1);
+  }
+  fs.readFile(f2Path, 'utf-8', (err, data2) => {
+    if (err) {
+      process.exit(1);
+    }
 
-		const concatdData = `${data1}${data2}`;
-		fs.writeFile(destPath, concatdData, err => {
-			if (err) {
-				process.exit(1);
-			}
-		});
-	});
+    const concatdData = `${data1}${data2}`;
+    fs.writeFile(destPath, concatdData, err => {
+      if (err) {
+        process.exit(1);
+      }
+    });
+  });
 });
