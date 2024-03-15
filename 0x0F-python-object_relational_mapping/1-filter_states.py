4b897c832c90a56542165198667cf452f135289f
Samia8Asim <samia2003.8@gmail.com>
2024-03-15 12:55:20 +0200

    0

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that lists all states from the database hbtn_0e_0_usa start with N"""
+"""script to lists all states from the database hbtn_0e_0_usa start with N"""
 import MySQLdb
 from sys import argv
 
