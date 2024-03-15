e004658ed89858653d23e67d7d0dbedfef9a8416
Samia8Asim <samia2003.8@gmail.com>
2024-03-15 13:28:13 +0200

    0

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id cities.name state.name FROM cities\
+    cur.execute("SELECT cities.id, cities.name, state.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
                 WHERE states.name = %s\
                 ORDER BY cities.id ASC", [argv[4]])
