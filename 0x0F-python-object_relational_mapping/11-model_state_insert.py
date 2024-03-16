c275b13f9b22eb63dfd24ce965c7ad6296e4e32f
Samia8Asim <samia2003.8@gmail.com>
2024-03-16 17:37:40 +0200

    0

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     new_state = State(name="Louisiana")
     session.add(new_state)
-    new_stat.commit()
+    session.commit()
 
     print(new_state.id)
     session.close()
